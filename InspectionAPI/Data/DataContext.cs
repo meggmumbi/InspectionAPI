21e624bec86bcd56765734d3f83367c58512b1b4
margaretmumbi16@gmail.com <margaretmumbi16@gmail.com>
2022-05-19 19:28:21 +0300

    Rest API

---
+++ InspectionAPI/Data/DataContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace InspectionAPI.Data
+{
+    public class DataContext : DbContext
+    {
+        //constructor
+        public DataContext(DbContextOptions<DataContext> options) : base(options) { }
+
+        public DbSet<Inspection> Inspections { get; set; }
+        public DbSet<InspectionType> InspectionTypes { get; set; }
+
+        public DbSet<Status> Statuses { get; set; }
+    }
+}
